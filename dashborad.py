9f73943b6f6466304ee9719195df741839e71253
Tommy Deng <tommy.deng2008@gmail.com>
2024-06-02 10:14:29 +1000

    dashboard added

---
+++ dashborad.py
@@ -0,0 +1,11 @@
+#User dashboard
+
+def menu():
+    print("Welcome to the user dashboard")
+    print("1. Add a new user")
+    print("2. Delete a user")
+    print("3. Update a user")
+    print("4. View all users")
+    print("5. Exit")
+
+
